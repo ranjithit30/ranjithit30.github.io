aad73c7111a9068a8fc7e995eaf28161a8d9342f
RanjithMunusamy <54244490+ranjithit30@users.noreply.github.com>
2023-11-21 11:40:49 +0530

    Delete grocery directory

--- grocery/script.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 10) {
-        $('#navbar').addClass('floatingNav');
-    } else {
-        $('#navbar').removeClass('floatingNav');
-    }
-});
